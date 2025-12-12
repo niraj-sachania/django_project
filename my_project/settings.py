0a80d6ae57bbe4b31b50ef3788d0a88943f0d58e
niraj-sachania <>
2025-12-12 01:07:02 +0000

    feat: allow localhost

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)(stgaw*@6@6^bxxi=n)!((!y%glg603h$p7$*x=%gokw6)7lw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition

8455aae1bee23ca0d94d3627cf50906bc3afbf39
niraj-sachania <>
2025-12-12 00:16:04 +0000

    feat: update index view to handle GET and POST requests

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    if request.method == "GET":
+        return HttpResponse("This is a GET request")
+    elif request.method == "POST":
+        return HttpResponse("This is a POST request")
